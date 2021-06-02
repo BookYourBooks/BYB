5d974a5eed822b2ca9823efd68cb8ac5ed4680a5
abhilashpujar <abhilashapujar@gmail.com>
2021-06-02 23:16:19 +0530

    added logout button

--- app/src/main/java/com/example/byb/HomeActivity.java
+++ app/src/main/java/com/example/byb/HomeActivity.java
@@ -3,9 +3,10 @@ package com.example.byb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private Button logoutbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
