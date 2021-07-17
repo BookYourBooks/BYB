abe0a4e4303f0f22e2bf020d97fda3c37e25ca9f
abhilashpujar <abhilashapujar@gmail.com>
2021-07-17 22:19:06 +0530

    modified document part so that admin can download pdf

--- app/src/main/java/com/example/byb/Admin/admin_printout_document.java
+++ app/src/main/java/com/example/byb/Admin/admin_printout_document.java
@@ -1,13 +1,10 @@
 package com.example.byb.Admin;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,31 +13,35 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.byb.Prevalent.Prevalent;
+import com.example.byb.HomeModel.testadapter;
 import com.example.byb.R;
-import com.example.byb.ViewHolder.CartViewHolder;
-import com.example.byb.ViewHolder.Documentviewholder;
-import com.example.byb.model.Cart;
-import com.example.byb.model.printout_document;
+import com.example.byb.ViewHolder.testviewholder;
+import com.example.byb.model.down_model;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 public class admin_printout_document extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private Button update;
     private TextView txtTotalPrice,confirmordermsg;
-
     private int overAllTotalPrice=0;
+
+    FirebaseFirestore db;
+    ArrayList<down_model> downModelArrayList = new ArrayList<>();
+    testadapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,52 +49,49 @@ public class admin_printout_document extends AppCompatActivity {
         setContentView(R.layout.activity_admin_printout_document);
 
 
-        recyclerView = findViewById(R.id.document_list);
+       setUpRV();
+        db= FirebaseFirestore.getInstance();
+        dataFromFirebase();
+
+    }
+
+    private void setUpRV() {
+        recyclerView= findViewById(R.id.document_list);
         recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
+    private void dataFromFirebase() {
+        if(downModelArrayList.size()>0)
+            downModelArrayList.clear();
 
 
-        final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("user_printout_document");
-        FirebaseRecyclerOptions<printout_document> options =
-                new FirebaseRecyclerOptions.Builder<printout_document>()
-                        .setQuery(cartListRef, printout_document.class)
-                        .build();
+        db.collection("user_printout_document")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        for(DocumentSnapshot documentSnapshot: task.getResult()) {
 
-        FirebaseRecyclerAdapter<printout_document, Documentviewholder> adapter
-                = new FirebaseRecyclerAdapter<printout_document, Documentviewholder>(options) {
-            @Override
-            protected void onBindViewHolder(@NonNull Documentviewholder holder, int i, @NonNull printout_document model)
-            {
-                holder.usn.setText(model.getUsn());
-                holder.copies.setText(" Copies = " +model.getNumberofcopies());
-                holder.link.setText(" Link = " +model.getGoogledrivelink());
-                holder.description.setText( model.getDescription());
+                            down_model downModel= new down_model(documentSnapshot.getString("usn"),documentSnapshot.getString("name"),documentSnapshot.getString("description"),
+                                    documentSnapshot.getString("url"),documentSnapshot.getString("numberofcopies"));
+                            downModelArrayList.add(downModel);
 
+                        }
 
+                        adapter= new testadapter(admin_printout_document.this,downModelArrayList);
+                        recyclerView.setAdapter(adapter);
+                    }
+                })
 
-            }
-
-            @NotNull
-            @Override
-            public Documentviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
-            {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.admin_document_item_list, parent ,false);
-                Documentviewholder  holder= new Documentviewholder (view);
-                return holder;
-            }
-        };
-        recyclerView.setAdapter(adapter);
-        adapter.startListening();
-
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(admin_printout_document.this, "Error ;-.-;", Toast.LENGTH_SHORT).show();
+                    }
+                })
+        ;
     }
+
 
 }
