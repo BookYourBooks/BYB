8762bd9cacc31ded7761604524d55797d1c8d583
Your Name <you@example.com>
2021-07-19 23:39:01 +0530

    Chat activity java file errors r there

---
+++ app/src/main/java/com/example/byb/ChatActivity.java
@@ -0,0 +1,169 @@
+package com.example.byb;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ChatActivity extends AppCompatActivity
+{
+    String ReceiverImage,ReceiverUID,ReceiverName,SenderUID;
+    CircleImageView profileImage;
+    TextView receiverName;
+    FirebaseDatabase database;
+    FirebaseAuth firebaseAuth;
+    public static String sImage;
+    public static String rImage;
+
+    CardView sendBtn;
+    EditText edtMessage;
+
+    String senderRoom,receiverRoom;
+
+    RecyclerView messageAdapter;
+    ArrayList<Messages> messagesArrayList;
+
+    MessagesAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        database = FirebaseDatabase.getInstance();
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        ReceiverName=getIntent().getStringExtra("ReceiverName");
+        ReceiverImage=getIntent().getStringExtra( name: "ReceiverImage");
+        ReceiverUID=getIntent().getStringExtra(name: "uid");
+
+        messagesArrayList=new ArrayList<>();
+
+        profileImage= findViewById(R.id.profile_image);
+        receiverName=findViewById(R.id.receiverName);
+
+        messageAdapter=findViewById(R.id.messageAdapter);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        messageAdapter.setLayoutManager(linearLayoutManager);
+        adapter=new MessagesAdapter(ChatActivity.this,messagesArrayList);
+        messageAdapter.setAdapter(adapter);
+
+        sendBtn=findViewById(R.id.sendBtn);
+        edtMessage=findViewById(R.id.edtMessage);
+
+        Picasso.get().load(ReceiverImage).into(profileImage);
+        receiverName.setText(""+ReceiverName);
+
+        SenderUID=FirebaseAuth.getUid();
+
+        senderRoom=SenderUID+ReceiverUID;
+        receiverRoom=ReceiverUID+SenderUID;
+
+
+
+        DatabaseReference reference = database.getReference().child("user").child(firebaseAuth.getUid());
+        DatabaseReference chatReference = database.getReference().child("chats").child(senderRoom).child("messages");
+
+
+        chatReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+
+                messagesArrayList.clear();
+
+                for(DataSnapshot dataSnapshot: snapshot.getChildren())
+                {
+                    Messages messages = dataSnapshot.getValue(Messages.class);
+                    messagesArrayList.add(messages);
+                }
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+        reference.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot)
+            {
+                sImage= snapshot.child("imageUri").getValue().toString();
+                rImage= ReceiverImage;
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error)
+
+                }
+            });
+
+        sendBtn.setOnClickListener(new View.OnClickListener()){
+            @Override
+                    public void onClick (View v) {
+                String message=edtMessage.getText().toString();
+                if(message.isEmpty())
+                {
+                    Toast.makeText(ChatActivity.this, "Please enter Valid Message", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                edtMessage.setText("");
+                Date date = new Date();
+
+                Messages messages= new Messages(message,SenderUID, date.getTime());
+
+                database = FirebaseDatabase.getInstance();
+                database.getReference().child("chats")
+                        .child(senderRoom)
+                        .child("messages")
+                        .push()
+                        .setValue(messages).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<Void> task) {
+                        database.getReference().child("chats")
+                                .child(receiverRoom)
+                                .child("messages")
+                                .push().setValue(messages).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull @NotNull Task<Void> task) {
+
+                            }
+                        });
+                    }
+                });
+
+        }
+    }
+
+        }
+    }
